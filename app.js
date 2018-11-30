8be5ac56cda45353cc64581a836784459bb669ac
Rokas Grigaravicius <grigroka@gmail.com>
2018-11-30 15:58:06 +0200

    constructor and data structure

--- app.js
+++ app.js
@@ -3,7 +3,22 @@
 
 // Item Controller
 const ItemCtrl = (function() {
-  console.log('item controller');
+  // Item Constructor
+  const Item = function(id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  };
+  // Data Structure / State
+  const data = {
+    items: [
+      { id: 0, name: 'Steak Dinner', calories: 1200 },
+      { id: 1, name: 'Cookie', calories: 400 },
+      { id: 2, name: 'Eggs', calories: 300 }
+    ],
+    currentItem: null,
+    totalCalories: 0
+  };
 })();
 // UI Controller
 const UICtrl = (function() {
