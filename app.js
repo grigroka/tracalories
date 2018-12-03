a1853370c8cabd6c88bf9f2ae7806e7e5097f8b6
Rokas Grigaravicius <grigroka@gmail.com>
2018-12-03 17:12:32 +0200

    load data from LS

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const StorageCtrl = (function() {
   // Public methods
   return {
     storeItem: function(item) {
-      let items = [];
+      let items;
 
       // Check if any items in ls
       if (localStorage.getItem('items') === null) {
@@ -25,9 +25,19 @@ const StorageCtrl = (function() {
         // Reset local storage
         localStorage.setItem('items', JSON.stringify(items));
       }
+    },
+    getItemsFromStorage: function() {
+      let items;
+      if (localStorage.getItem('items') === null) {
+        items = [];
+      } else {
+        items = JSON.parse(localStorage.getItem('items'));
+      }
+      return items;
     }
   };
 })();
+
 // ITEM CONTROLLER
 const ItemCtrl = (function() {
   // Item Constructor
@@ -39,11 +49,12 @@ const ItemCtrl = (function() {
 
   // Data Structure / State
   const data = {
-    items: [
-      // { id: 0, name: 'Steak Dinner', calories: 1200 },
-      // { id: 1, name: 'Cookie', calories: 400 },
-      // { id: 2, name: 'Eggs', calories: 300 }
-    ],
+    // items: [
+    //   { id: 0, name: 'Steak Dinner', calories: 1200 },
+    //   { id: 1, name: 'Cookie', calories: 400 },
+    //   { id: 2, name: 'Eggs', calories: 300 }
+    // ],
+    items: StorageCtrl.getItemsFromStorage(),
     currentItem: null,
     totalCalories: 0
   };
